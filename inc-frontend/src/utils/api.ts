73729948f1baf1f0531b4339a5c86d887fe26b0f
vamshi-krishna-vijayagiri <vamshi.vijayagiri@gmail.com>
2026-02-18 12:45:50 +0530

    Docker URL Updated

--- inc-frontend/src/utils/api.ts
+++ inc-frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL;
+  process.env.REACT_APP_API_URL || "http://192.168.1.178:5000/api";
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
